ab7c1c18d471802957fe5aef0b6e9fa329babce8
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2025-01-22 09:53:10 +0530

    Add files via upload

---
+++ pattern printing.c
@@ -0,0 +1,41 @@
+/*
+* * * * 
+* * * *
+* * * * 
+* * * *
+
+
+#include <stdio.h>
+void main(){
+    int i,j;
+    for (i=0;i<4;i++){
+        for (j=0;j<3;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+*/
+
+/*
+
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+
+#include <stdio.h>
+int main(){
+    int i,j;
+    for (i=0;i<5;i++){
+        for (j=0;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+*/
