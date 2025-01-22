c577b27bca0adbd9ce4f08f58870582aa35d205c
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2025-01-22 10:11:00 +0530

    Update pattern printing.c

--- pattern printing.c
+++ pattern printing.c
@@ -39,3 +39,51 @@ int main(){
     return 0;
 }
 */
+
+/*
+A 
+B A
+A B A
+B A B A
+A B A B A
+
+
+#include <stdio.h>
+int main(){
+    int i,j;
+    for (i=0;i<5;i++){
+        for (j=0;j<=i;j++){
+            if ((i+j)%2==0){
+                printf("A ");
+            }
+            else{
+                printf("B ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+*/
+/*
+1 
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+
+
+#include <stdio.h>
+int main(){
+    int i,j;
+    for (i=1;i<6;i++){
+        for (j=1;j<=i;j++){
+        printf("%d ",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+*/
+
