6a834827967dc585cec8a1d682cbd608b8bbdc63
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2025-01-23 11:07:05 +0530

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -17,7 +17,7 @@ importanr--- *(ptr) means *(&i) means value at the address of i , i.e *2345 mean
 so a=*(&i) means a=3
 eg program
 */
-
+/*
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -29,5 +29,46 @@ int main(){
     printf("%p\n",&i);
     printf("%p\n",*ptr);
     printf("%p\n",i);
+    return 0;
+}
+*/
+
+/*while declaring function with arguments as pointer of int type
+we dec;are as 
+void swap(int*,int*); instead of int,int normal
+void swap(int *ptr1,int *ptr2){
+....
+<body of function>
+....
+}
+
+
+so by using pointers, i can directly work with memory addresses. Means i can use it anywhere in any function,or in main.
+
+also
+*ptr1=72 is equivalent to *(&a)=72....means value at address of a =72.
+*/
+
+
+
+#include <stdio.h>
+
+int area(int *ptr1, int *ptr2)
+ {
+    return (*ptr1) * (*ptr2);
+}
+
+int main() {
+    int a, b, *ptr1, *ptr2,result;
+    printf("Enter length and breadth: ");
+    scanf("%d%d", &a, &b);
+    
+    ptr1 = &a;
+    ptr2 = &b;
+    
+    result = area(ptr1, ptr2);
+
+    printf("Area: %d\n", result);
+
     return 0;
 }
