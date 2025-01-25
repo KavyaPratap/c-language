0891ad29c4ac253395f3d23286ad058c82414a69
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2025-01-25 11:41:26 +0530

    Update transpose.c

--- transpose.c
+++ transpose.c
@@ -2,23 +2,21 @@
 #define MAX 100
 
 int main(){
-	int rows,columns,i,j,a[MAX][MAX],b[MAX][MAX],sum=0,temp;
+	int rows,columns,i,j,a[MAX][MAX],c[MAX][MAX];
 	printf("enter number of rows and columns\n");
 	scanf("%d%d",&rows,&columns);
+	printf("Matrix---\n");
 	for (i=0;i<rows;i++){
 		for (j=0;j<columns;j++){
 			scanf("%d",&a[i][j]);
-			b[j][i]=a[i][j];
+			c[j][i]=a[i][j];
 		}
 	}
-
-
-	printf("\nTranspose\n");
+	printf("\nTranspose of matrix---\n");
 	for (j=0;j<columns;j++){
 		for (i=0;i<rows;i++){
-			printf("%d ",b[j][i]);
+			printf("%d ",c[j][i]);
 		}
 		printf("\n");
+		}	
 	}
-
-}
