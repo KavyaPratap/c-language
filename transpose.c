fa042c472414e6996e4da37697bd07642f386f4f
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2025-01-25 11:39:37 +0530

    Create transpose.c

---
+++ transpose.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define MAX 100
+
+int main(){
+	int rows,columns,i,j,a[MAX][MAX],b[MAX][MAX],sum=0,temp;
+	printf("enter number of rows and columns\n");
+	scanf("%d%d",&rows,&columns);
+	for (i=0;i<rows;i++){
+		for (j=0;j<columns;j++){
+			scanf("%d",&a[i][j]);
+			b[j][i]=a[i][j];
+		}
+	}
+
+
+	printf("\nTranspose\n");
+	for (j=0;j<columns;j++){
+		for (i=0;i<rows;i++){
+			printf("%d ",b[j][i]);
+		}
+		printf("\n");
+	}
+
+}
